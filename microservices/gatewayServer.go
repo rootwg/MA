d47b3fad88db741b78fbcf97e22218239c5bcada
wanggang <18672349266@163.com>
2023-06-16 17:42:45 +0800

    Initial commit

--- microservices/gatewayServer.go
+++ microservices/gatewayServer.go
@@ -2,31 +2,44 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"studentDemo/sys/router"
 )
+
+type User struct {
+	Name string
+}
 
 func main() {
 	//modoule
 	engine := router.NewEngine("8080")
 	//添加分组user
 	userGroup := engine.Group("user")
-	userGroup.AddGet("/hello", func(ctx *router.Context) {
-		fmt.Fprintln(ctx.W, "get user hello mszlu.com")
-	})
-	userGroup.AddGet("/hello2", func(ctx *router.Context) {
-		fmt.Fprintln(ctx.W, "get user hello2  mszlu.com")
+	userGroup.AddGet("/index", func(ctx *router.Context) {
+		fmt.Println("index handler")
+		user := &User{
+			Name: "go微服务框架",
+		}
+		ctx.HtmlTemplate("login.html", template.FuncMap{}, user, "D:\\spaceWork\\go\\MA\\microservices\\sys\\router\\tpl\\login.html", "D:\\spaceWork\\go\\MA\\microservices\\sys\\router\\tpl\\header.html")
 	})
 
-	orderGroup := engine.Group("order")
-	orderGroup.AddGet("/hello", func(ctx *router.Context) {
-		fmt.Fprintln(ctx.W, "get order hello  mszlu.com")
-	})
-	orderGroup.AddGet("/hello3", func(ctx *router.Context) {
-		fmt.Fprintln(ctx.W, "get order hello3  mszlu.com")
-	})
-	orderGroup.AddGet("/hello2/order", func(ctx *router.Context) {
-		fmt.Fprintln(ctx.W, "get hello/order hello2  mszlu.com")
-	})
+	//userGroup.AddGet("/hello", func(ctx *router.Context) {
+	//	fmt.Fprintln(ctx.W, "get user hello mszlu.com")
+	//})
+	//userGroup.AddGet("/hello2", func(ctx *router.Context) {
+	//	fmt.Fprintln(ctx.W, "get user hello2  mszlu.com")
+	//})
+	//
+	//orderGroup := engine.Group("order")
+	//orderGroup.AddGet("/hello", func(ctx *router.Context) {
+	//	fmt.Fprintln(ctx.W, "get order hello  mszlu.com")
+	//})
+	//orderGroup.AddGet("/hello3", func(ctx *router.Context) {
+	//	fmt.Fprintln(ctx.W, "get order hello3  mszlu.com")
+	//})
+	//orderGroup.AddGet("/hello2/order", func(ctx *router.Context) {
+	//	fmt.Fprintln(ctx.W, "get hello/order hello2  mszlu.com")
+	//})
 
 	//启动
 	engine.Run()
--- microservices/sys/router/context.go
+++ microservices/sys/router/context.go
@@ -1,8 +1,30 @@
 package router
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 type Context struct {
 	W http.ResponseWriter
 	R *http.Request
+}
+
+//为html模板设置值 并返回
+func (c *Context) HtmlTemplate(name string, funcMap template.FuncMap, data any, fileName ...string) {
+	t := template.New(name)
+	t.Funcs(funcMap)
+
+	files, err := t.ParseFiles(fileName...)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+	c.W.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err = files.Execute(c.W, data)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
 }
